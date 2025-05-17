5d78cd01eb4a90cf6fd069039c0a839c4fcd714a
hassantoufeeq30 <hassantoufeeq30@gmail.com>
2025-05-17 00:32:07 -0700

    Update sets.py

--- set.py
+++ set.py
@@ -1,4 +1,4 @@
-"""num = {1, 2, 2, 2, 2, 2, 3, 4, 5, 6, "jan", "khan"}
+num = {1, 2, 2, 2, 2, 2, 3, 4, 5, 6, "jan", "khan"}
 print(num)
 print(len(num))
 print(num.add(7))
@@ -39,7 +39,7 @@ print(set2.intersection(set1))
 set3 = {"python", "c++", "java", "javscipt",
         "python", "c++", "java", "javscipt"}
 print(len(set3))
-"""
+
 values = {
     ('float', 9.0),
     ('int', 9)
